c57e1a17eeae614ef87bdb2edfb593ed76565cb4
Grazielly Varanda <vieiravarandagrazielly@gmail.com>
2024-08-23 20:39:38 -0300

    create MyApp

---
+++ App/app/src/main/java/br/com/example/myapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package br.com.example.myapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.example.myapp.ui.theme.MyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var txtFieldValue = rememberSaveable {
+                mutableStateOf("")
+            }
+            Column {
+                Text(text = "text")
+                Text(text = "text")
+                Text(text = "text")
+                TextField(value = txtFieldValue.value, onValueChange = {
+                    txtFieldValue.value = it
+                    Log.i("###", it)
+                })
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyAppTheme {
+        Greeting("Android")
+    }
+}
